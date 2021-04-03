4c6b21cd2efef873afc6b8bd2b39ded9bd4a2067
hoffa95 <hoffaboy@gmail.com>
2021-04-03 10:13:56 +0200

    Change welcome msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Hoffaa!
+          Hello from AWS Hoffaa!
         </p>
         <a
           className="App-link"
